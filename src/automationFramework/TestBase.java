d03d7f6aea90e17b6f624cc9ee0d44730654bdc8
erwinbooy <lamp1973@hotmail.com>
2016-05-09 09:23:00 +0200

    small changes

--- src/automationFramework/TestBase.java
+++ src/automationFramework/TestBase.java
@@ -1,14 +1,5 @@
 package automationFramework;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
-import org.junit.Rule;
-import org.junit.rules.TestRule;
-import org.junit.rules.TestWatcher;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -17,30 +8,6 @@ import junit.framework.TestCase;
 public abstract class TestBase extends TestCase {
 
 	private FirefoxDriver myDriver;
-
-
-	/**
-	 * We create this rule to have a screenshot in case the test fails
-	 */
-    @Rule
-    public TestRule screenshotRule = new TestWatcher() {
-    	
-        @Override
-        protected void failed(Throwable e, org.junit.runner.Description description) {
-            captureScreenshot(description.getMethodName());
-        }
-        
-        private void captureScreenshot(String name) {
-            File screenshot = ((TakesScreenshot) myDriver)
-                    .getScreenshotAs(OutputType.FILE);
-            String path = "./target/screenshots/" + name +"_"+ screenshot.getName();
-            try {
-                FileUtils.copyFile(screenshot, new File(path));
-            } catch (IOException e) {
-
-            }
-        }
-    };
 	
 	/**
 	 * setUp method which is used by the Test framework
